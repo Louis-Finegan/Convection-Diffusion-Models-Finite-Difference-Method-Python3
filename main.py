ad3be909c36c820d32fffb34a6be796fa3f7ab98
Louis-Finegan <louis02finegan@gmail.com>
2023-01-17 19:34:48 +0000

    # Changes: initial condition changed

--- main.py
+++ main.py
@@ -48,13 +48,14 @@ if __name__ == '__main__':
     print('-------------------- \nSCRIPT HAS STARTED \n--------------------')
     dx = 0.01
     dt = 0.01
-    x = np.arange(-5, 5, dx)
-    y = np.arange(-5, 5 ,dx)
+    x = np.arange(-2, 2, dx)
+    y = np.arange(-2, 2 ,dx)
     X, Y = np.meshgrid(x, y)
-    init = 4*(1/np.sqrt(0.01*2*np.pi))*np.exp(-(1/2)*((X**2 + Y**2)/0.01))
+    #init = 4*(1/np.sqrt(0.01*2*np.pi))*np.exp(-(1/2)*((X**2 + Y**2)/0.01))
+    init = np.heaviside(X, 1) * np.heaviside(Y, 1)
     heat_array = d.diffusion_2dims(100, 100, dt, dx, 0.1, init)
     print('OBJECT INITIALIZED SUCCESSFULLY \n--------------------')
-    solution = heat_array.solve_Dirichlet(boundary=[0,0,0,0])
+    solution = heat_array.solve_Neumann(boundary_flux=[0,0,0,0])
     print(time.time())
     if isinstance(solution, np.ndarray):
         print('SOLUTION FOUND')
