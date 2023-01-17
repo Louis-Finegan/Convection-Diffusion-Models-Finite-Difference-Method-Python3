45d45e6de79d41e5581fe43018bbc58a4ee9ef8b
Louis-Finegan <louis02finegan@gmail.com>
2023-01-17 20:22:15 +0000

    changes: convection.py added changes made to diffusion.py

---
+++ diffuconpy/convection.py
@@ -0,0 +1,10 @@
+
+# Numpy is used
+import numpy
+
+class convection_2dims:
+    pass
+
+class convection_1dims:
+    pass
+
--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib.animation as animation
-import diffusion as d
+import diffuconpy.diffusion as d
 import time
 
-# Animation function for 2 dimensional solution
+# Animates the diffusion process in 2 dimensions
 def animation_(solution, X, Y, fps, frn, filename) -> None:
     plt.rcParams["figure.figsize"] = [10, 10]
     plt.rcParams["figure.autolayout"] = True
@@ -25,7 +25,7 @@ def animation_(solution, X, Y, fps, frn, filename) -> None:
     plt.close()
     ani.save(filename, writer='pillow', fps=fps)
 
-# Animation function for the 1 dimensional solution
+# Animates the diffusion process in 1 dimension
 def animation_1(solution, X, fps, frn, filename):
         fig = plt.figure()
         ax = plt.axes(xlim=(-5, 5), ylim=(-5, 10)) # left bound -5 and right bound 5
@@ -43,19 +43,23 @@ def animation_1(solution, X, fps, frn, filename):
         plt.close()
         anim.save(filename, writer='pillow', fps=fps)
 
+# Animates the diffusion process in 2 dimensions but as a color plot
+def animation_color():
+    pass
 
 if __name__ == '__main__':
+    # Uses diffusion modules to solve with initial condition `init` and parameters given
     print('-------------------- \nSCRIPT HAS STARTED \n--------------------')
-    dx = 0.01
-    dt = 0.01
+    dx = 0.1
+    dt = 0.1
     x = np.arange(-2, 2, dx)
     y = np.arange(-2, 2 ,dx)
     X, Y = np.meshgrid(x, y)
     #init = 4*(1/np.sqrt(0.01*2*np.pi))*np.exp(-(1/2)*((X**2 + Y**2)/0.01))
     init = np.heaviside(X, 1) * np.heaviside(Y, 1)
-    heat_array = d.diffusion_2dims(100, 100, dt, dx, 0.1, init)
+    heat_array = d.diffusion_2dims(200, 50, dt, dx, 0.1, init)
     print('OBJECT INITIALIZED SUCCESSFULLY \n--------------------')
-    solution = heat_array.solve_Neumann(boundary_flux=[0,0,0,0])
+    solution = heat_array.solve_Dirichlet(boundary=[0,0,0,0])
     print(time.time())
     if isinstance(solution, np.ndarray):
         print('SOLUTION FOUND')
